439e9757ecc67c7e3089f05eccc188a023d853bf
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-05-12 20:12:34 -0300

    add blur header

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -5,6 +5,35 @@ import MenuSideBar from '../MenuSideBar/MenuSideBar'
 
 const Header = () => {
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+  const [scrollPos, setScrollPos] = React.useState(0);
+  const [classe, setClasse] = React.useState('')
+
+  React.useEffect(() => {
+    let prevScrollPos = window.pageYOffset;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setScrollPos(currentScrollPos);
+      if(currentScrollPos < 100){
+        setClasse('')
+      }
+      else if(currentScrollPos > prevScrollPos) {
+        setClasse('hidden')
+        console.log('baixo')
+      } else {
+        setClasse('bgBlur')
+      }
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollPos]);
+
 
   React.useEffect(() => {
      const handleResize = () => {
@@ -20,7 +49,7 @@ const Header = () => {
   }, []);
 
   return (
-    <header className='flex w-full justify-center fixed z-30'>
+    <header className={`flex w-full justify-center fixed z-30 ${classe}`}>
       <div className='flex w-full max-w-[1920] py-2 px-4 justify-between items-center'>
         <img className='w-72' src={Logo} alt="Logo Bocarra Circus" />
 
