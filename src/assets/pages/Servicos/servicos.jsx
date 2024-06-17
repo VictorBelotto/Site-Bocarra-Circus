b2d2946f61fbd17eea0e89ef2b9ad06ab0d91b06
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-06-17 20:45:48 -0300

    correção build

--- src/assets/pages/Servicos/servicos.jsx
+++ src/assets/pages/Servicos/servicos.jsx
@@ -14,11 +14,11 @@ const Servicos = () => {
   return (
     <main className='flex flex-col w-full items-center mb-32'>
       <div className='flex w-full bg-blue-default justify-center'>
-        <div className='flex flex-col w-[95%] max-w-7xl grande:max-w-[1380px] min-h-[90vh] py-16 mt-16'>
+        <div className='flex flex-col w-[95%] max-w-7xl grande:max-w-[1380px] h-[calc(100svh)]  pt-36'>
           <Separador />
           <h2 className='text-[white] mb-8 md:mb-16 lg:mb-8'>Soluções Personalizadas para Cada Projeto</h2>
           <p className='max-w-[800px] text-white-contraste text-xl sm:text-2xl ml-4'>Na Bocarra Circus, oferecemos soluções personalizadas em coberturas têxteis, combinando tradição e inovação para atender às necessidades únicas de cada cliente.</p>
-          <div className='flex gap-2 mt-auto'>
+          <div className='flex gap-2 mt-auto mb-4'>
             <img className='w-4 ml-4' src={mouseIcon} alt='Icone Mouse' />
             <p className='text-white-contraste'>Continue navegando</p>
           </div>
