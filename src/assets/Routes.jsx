fffd1133d2d22e4e8a79ca92b0b22c289615ff39
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-06-17 21:11:15 -0300

    Update Routes.jsx

--- src/assets/Routes.jsx
+++ src/assets/Routes.jsx
@@ -4,7 +4,6 @@ import PageBase from './pages/PageBase/PageBase.jsx'
 import Home from './pages/Home/Home.jsx'
 import Portifolio from './pages/Portifolio/Portifolio.jsx'
 import Sobre from './pages/Sobre/Sobre.jsx'
-import Servicos from './pages/Servicos/Servicos'
 import ScrollToTop from './components/ScrollTop/ScrollTop.jsx'
 
 
