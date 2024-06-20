e3933332ee2c0bc7ddfb3fe1e33b302a1afc688e
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-06-19 23:56:11 -0300

    add new carousel home

--- src/assets/pages/Home/components/CarrosselPrincipal/CarrosselPrincipal.jsx
+++ src/assets/pages/Home/components/CarrosselPrincipal/CarrosselPrincipal.jsx
@@ -1,70 +1,53 @@
-import React from 'react';
-import img1 from '../../../../images/imagens_carrossel/1.webp'
-import img2 from '../../../../images/imagens_carrossel/2.webp'
-import img3 from '../../../../images/imagens_carrossel/3.webp'
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { motion } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
+import img1 from '../../../../images/imagens_carrossel/1.webp';
+import img2 from '../../../../images/imagens_carrossel/2.webp';
+import img3 from '../../../../images/imagens_carrossel/3.webp';
+import { Carousel } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
+import { Autoplay } from "@fancyapps/ui/dist/carousel/carousel.autoplay.esm.js";
+import "@fancyapps/ui/dist/carousel/carousel.css";
+import "@fancyapps/ui/dist/carousel/carousel.autoplay.css";
 
 const CarrosselPrincipal = () => {
-  const images = [img1, img2, img3]
-  const progressCircle = React.useRef(null);
-  const progressContent = React.useRef(null);
+  const carouselRef = useRef(null);
+  const images = [img1, img2, img3];
 
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    if (progressCircle.current) {
-      progressCircle.current.style.setProperty('--progress', 1 - progress);
-    }
-    if (progressContent.current) {
-      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-    }
-  };
+  useEffect(() => {
+    // Configuração do carousel com autoplay
+    const carousel = new Carousel(carouselRef.current, {
+      Autoplay: {
+        timeout: 4000,
+        pauseOnHover: false
+      },
+      Dots: {
+        classes: {
+           list: "flex gap-2 absolute w-full justify-center bottom-0 text-[white]",
+           isCurrent: "text-red-default",
+        }
+      },
+    }, {
+      Autoplay,
+
+    });
+
+    return () => {
+      carousel.destroy();
+    };
+  }, []);
 
   return (
-    <section className='flex w-full h-svh'>
-      <Swiper
-        className='swiperHome hover:cursor-grab grap'
-        spaceBetween={0}
-        centeredSlides={true}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        loop={true}
-      >
+    <section className=' w-full h-screen '>
+      <div ref={carouselRef} className='f-carousel '>
         {images.map((imagem, index) => (
-          <SwiperSlide key={index} className='relative overflow-hidden '>
-            <motion.img
+          <div key={index} className='relative f-carousel__slide'>
+            <img
               src={imagem}
               loading='lazy'
-              className='w-screen h-screen aspect-[9/16] object-cover flex items-center justify-center '
-              initial={{ opacity: 0 }}
-              animate={{ opacity: [0.2, 0.4,0.6,0.8, 1] }}
-              transition={{ duration: 1.5 }}
+              className='w-screen h-[100svh] object-cover'
+              alt={`Imagem ${index + 1}`}
             />
-          </SwiperSlide>
+          </div>
         ))}
-        <motion.div 
-          className='autoplayProgress' slot="container-end"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent} className='text-slate-100'></span>
-        </motion.div>
-      </Swiper>
+      </div>
     </section>
   );
 };
