ab7f60711f2c8e7cba6d5cc05dc98ad036013089
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-06-25 23:15:05 -0300

    remove component especialista

--- src/assets/pages/Sobre/Sobre.jsx
+++ src/assets/pages/Sobre/Sobre.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import Especialista from './components/Especialista'
 
 const Sobre = () => {
   return (
     <div className='flex flex-col w-full items-center '>
-      <Especialista/>
       
     </div>
   )
