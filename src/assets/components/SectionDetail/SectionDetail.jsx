ca4ef479c3aeb10cda102265df34953426f4201e
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-06-26 17:11:06 -0300

    style

--- src/assets/components/SectionDetail/SectionDetail.jsx
+++ src/assets/components/SectionDetail/SectionDetail.jsx
@@ -8,10 +8,11 @@ import SliderOtherServices from './components/sliderOtherServices/sliderOtherSer
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import Gallery from './components/gallery/Gallery.jsx';
 import ContinueNavegando from  '../../components/continueNavegando/ContinueNavegando.jsx'
+import { motion  } from "framer-motion";
 
 const SectionDetail = () => {
   const { id } = useParams();
-
+  
   const section = sectionsData.find(section => section.id === id);
 
   useEffect(() => {
@@ -31,10 +32,19 @@ const SectionDetail = () => {
   }
 
   return (
-    <div className='flex flex-col w-full justify-center items-center pb-32' key={section.id}>
+    <motion.main 
+      className='flex flex-col w-full justify-center items-center pb-32' 
+      key={section.id}
+      initial="hidden"
+      animate="visible"
+      exit={{ opacity: 0, transition: { duration: 0.5 } }}
+    >
       <div className='bg-blue-default flex flex-col w-full min-h-[100vh] items-center'>
         <div className='w-maxW max-w-hd grande:max-w-grande absolute top-[calc(55svh-128px)] md:top-[calc(65svh-128px)]'>
-            <h2 className='text-[white] w-fit bgBlur px-4'>{section.titulo}</h2>
+            <motion.h2  variants={{
+          hidden: { opacity: 0, y: -20 },
+          visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
+        }} className='text-[white] w-fit bgBlur px-4'>{section.titulo}</motion.h2>
         </div>
 
         <img
@@ -59,7 +69,7 @@ const SectionDetail = () => {
 
         <SliderOtherServices section={section} sectionsData={sectionsData} />
       </div>
-    </div>
+    </motion.main>
   );
 };
 
