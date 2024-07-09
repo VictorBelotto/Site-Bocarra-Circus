7adc2a9b016068736616756ae6ce9f54a3d34252
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-07-09 11:21:37 -0300

    style: add motion page services

--- src/assets/pages/Servicos/Services.jsx
+++ src/assets/pages/Servicos/Services.jsx
@@ -2,20 +2,44 @@ import Card from './components/Card/Card';
 import { servicesData } from './servicesData/servicesData';
 import Separador from '../../components/Separador/Separador';
 import ContinueNavegando from '../../components/continueNavegando/ContinueNavegando';
+import { motion } from 'framer-motion'
 
 const Services = () => {
+  const container = {
+    hidden: { opacity: 1 },
+    visible: {
+      transition: {
+        delayChildren: 0.35,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  
+
+  const item = {
+    hidden: { y: '200%', opacity: 0 },
+    visible: { y: ['100%', 0], opacity: 1, transition: { duration: 0.5 } },
+  };
+
   return (
     <main className='flex flex-col w-full items-center mb-32'>
-      <div className='flex w-full bg-blue-default justify-center'>
-        <div className='flex flex-col w-maxW max-w-hd grande:max-w-grande h-screen pt-32'>
+      <motion.div
+        className='flex w-full bg-blue-default justify-center '
+        variants={container}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <div className='flex flex-col w-maxW max-w-hd grande:max-w-grande h-screen pt-32 overflow-hidden'>
           <Separador />
-          <h2 className='text-[white] mb-8 md:mb-16 lg:mb-8'>Soluções Personalizadas para Cada Projeto</h2>
-          <p className='max-w-[1000px] text-white-contraste text-xl sm:text-2xl ml-4'>
+          <motion.h2 className='text-[white] mb-8 md:mb-16 lg:mb-8' variants={item} >Soluções personalizadas para cada projeto</motion.h2>
+          <motion.p className='max-w-[1000px] text-white-contraste text-xl sm:text-2xl ml-4' variants={item}>
             Na Bocarra Circus, oferecemos soluções personalizadas em coberturas têxteis, combinando tradição e inovação para atender às necessidades únicas de cada cliente.
-          </p>
-          <ContinueNavegando/>
+          </motion.p>
+          <ContinueNavegando />
         </div>
-      </div>
+      </motion.div>
 
       {servicesData.map((data) => (
         <Card
--- src/assets/pages/Servicos/components/Card/Card.jsx
+++ src/assets/pages/Servicos/components/Card/Card.jsx
@@ -1,31 +1,65 @@
 import { Link } from 'react-router-dom'
 import Separador from '../../../../components/Separador/Separador'
+import { motion } from 'framer-motion'
 
 const Card = ({ img, titulo, descricao, to, variant }) => {
+  const container = {
+    hidden: { opacity: 1 },
+    visible: {
+      transition: {
+        delayChildren: 0.35,
+        staggerChildren: 0.35,
+      },
+    },
+  };
+  const item = {
+    hidden: { x: '-200%', opacity: 0 },
+    visible: { x: ['-100%', 0], opacity: 1, transition: { duration: 0.39 } },
+  };
+  const item2 = {
+    hidden: { x: '200%', opacity: 0 },
+    visible: { x: ['100%', 0], opacity: 1, transition: { duration: 0.39 } },
+  };
+
   const styles = {
     blue: {
       container: 'bg-blue-default',
       title: 'text-[white]',
       text: 'text-white-contraste',
-      direction: 'lg:flex-row-reverse'
+      direction: 'lg:flex-row-reverse',
+      motion_img: item2,
+      motion_text: item
     },
     default: {
       container: '',
       title: 'text-black-text',
       text: 'text-gray-text',
-      direction: 'lg:flex-row'
+      direction: 'lg:flex-row',
+      motion_img: item,
+      motion_text: item2
     },
   }
   const style = variant ? styles.blue : styles.default
 
   return (
-    <main className={`flex w-full justify-center ${style.container}`}>
+    <motion.main
+      className={`flex w-full justify-center ${style.container}`}
+      variants={container}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.48 }}
+    >
       <section className={`flex flex-col w-maxW max-w-hd grande:max-w-grande justify-between py-16`}>
         {variant && (<Separador />)}
-        <main className={`flex flex-col w-full lg:justify-between ${style.direction} gap-4 items-center`}>
-          <img src={img} alt={titulo} className='w-full max-w-[700px] max-h-[400px] bg-gray-700 object-cover' />
+        <main className={`flex flex-col w-full lg:justify-between ${style.direction} gap-4 items-center overflow-hidden`}>
+          <motion.img
+            src={img}
+            alt={titulo}
+            className='w-full max-w-[700px] max-h-[400px] bg-gray-700 object-cover'
+            variants={style.motion_img}
+          />
 
-          <div className='flex flex-col mt-8 gap-8 lg:mt-0'>
+          <motion.div className='flex flex-col mt-8 gap-8 lg:mt-0' variants={style.motion_text}>
             <h3 className={style.title}>{titulo}</h3>
             <p className={`indent-4 w-[95%] lg:max-w-[440px] ${style.text}`}>
               {descricao}
@@ -36,10 +70,10 @@ const Card = ({ img, titulo, descricao, to, variant }) => {
             >
               veja mais
             </Link>
-          </div>
+          </motion.div>
         </main>
       </section>
-    </main>
+    </motion.main>
   )
 }
 
