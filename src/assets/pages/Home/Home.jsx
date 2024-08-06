21c6cba08cd165e92556423b3a41e222969ca8cf
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-08-06 16:15:33 -0300

    teste metadata homepage

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -11,10 +11,17 @@ import { Helmet } from 'react-helmet';
 const Home = () => {
   return (
     <main className='flex flex-col w-full items-center'>
-      <Helmet>
-        <title>Home</title>
+         <Helmet>
+        <title>Home - Bocarra Circus</title>
         <meta name="description" content="Alguma descrição Home" />
-        <meta property="og:title" content="MyApp" />
+        <meta name="keywords" content="Circo, Lonas, Bocarra Circus, Tradição, Inovação, bocarra circus site, homepage bocarra, bocarra, circus brasil, fabricante de lonas" />
+        <meta name="author" content="Bocarra Circus" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.bocarracircus.com" />
+        <meta property="og:title" content="Bocarra Circus - Tradição e Inovação em Lonas Circenses" />
+        <meta property="og:description" content="Com mais de 30 anos de experiência, a Bocarra Circus oferece lonas circenses de alta qualidade, unindo tradição e inovação para encantar e inspirar o público." />
+        <meta property="og:image" content="../../images/bocarra_visual/rino.svg" />
       </Helmet>
       <CarrosselPrincipal />
       <Slogan />
