d8e7e53b14c2a52addd337634498f0b406ac6d79
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-08-08 22:34:33 -0300

    style

--- src/assets/Routes.jsx
+++ src/assets/Routes.jsx
@@ -1,10 +1,7 @@
-// src/AppRoutes.js
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PageBase from './pages/PageBase/PageBase.jsx';
 import Home from './pages/Home/Home.jsx';
 import Portifolio from './pages/Portifolio/Portifolio.jsx';
-import Sobre from './pages/Sobre/Sobre.jsx';
-import ScrollToTop from './components/ScrollTop/ScrollTop.jsx';
 import Services from './pages/Servicos/Services.jsx';
 import SectionDetail from './components/SectionDetail/SectionDetail.jsx';
 import Contato from './pages/contato/Contato.jsx';
@@ -14,20 +11,19 @@ import SectionLonas from './components/SectionLonas/SectionLonas.jsx';
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <ScrollToTop />
       <Transition>
-        <Routes>
-          <Route path='/' element={<PageBase />}>
-            <Route index element={<Home />} />
-            <Route path='/portifolio' element={<Portifolio />} />
-            {/* <Route path='/sobre' element={<Sobre />} /> */}
-            <Route path='/servicos' element={<Services />} />
-            <Route path='/servicos/:id' element={<SectionDetail />} />
-            <Route path='/circo/:id' element={<SectionLonas />} />
-            <Route path='/contato' element={<Contato />} />
-          </Route>
-        </Routes>
-        
+      <Routes>
+        <Route path='/' element={<PageBase />}>
+          <Route index element={<Home />} />
+          <Route path='/portifolio' element={<Portifolio />} />
+          {/* <Route path='/sobre' element={<Sobre />} /> */}
+          <Route path='/servicos' element={<Services />} />
+          <Route path='/servicos/:id' element={<SectionDetail />} />
+          <Route path='/circo/:id' element={<SectionLonas />} />
+          <Route path='/contato' element={<Contato />} />
+        </Route>
+      </Routes>
+     
       </Transition>
     </BrowserRouter>
   );
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
 import escritaBranca from '../../images/bocarra_visual/escritaBranca.svg';
@@ -14,9 +13,6 @@ const Header = () => {
     visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
   };
 
-  useEffect(() => {
-  }, [location.pathname]);
-
   return (
     <div className='relative w-full'>
       <div className={`w-full z-20 bg-transparent absolute top-0`}>
@@ -27,7 +23,7 @@ const Header = () => {
           variants={variants}
           key={location.pathname}
         >
-          <div className='flex w-maxW max-w-hd grande:max-w-grande py-2 justify-between items-center'>
+          <div className='flex w-maxW max-w-hd grande:max-w-grande py-2 justify-between items-center relative'>
             <Logo escrita={escritaBranca} />
             {window.innerWidth < 968 ? <MenuSideBar /> : <Nav />}
           </div>
